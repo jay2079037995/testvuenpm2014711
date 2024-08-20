f5f97a118532cae9343edb3f1795431d67541238
jay2079037995 <157499996+jay2079037995@users.noreply.github.com>
2024-08-20 16:12:37 +0800

    aaa

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <pig-button></pig-button>
+  </div>
+</template>
+ 
+<script>
+import PigButton from './package/pig-button/index.vue'
+ 
+export default {
+  name: 'App',
+  components: {
+    PigButton
+  }
+}
+</script>
+ 
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
